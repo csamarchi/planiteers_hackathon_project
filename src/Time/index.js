5963da54a0bb8f406bf576ee2ad24c2e46337fcf
Christine Samarchi <csamarchi@gmail.com>
2018-12-21 09:08:35 -0600

    drop down for time form on time and date page

--- src/Time/index.js
+++ src/Time/index.js
@@ -14,35 +14,63 @@ class Time extends Component {
         <Nav />
         <div>
           <h1 className='header' > Choose a Time </h1>
-
-          <h3> Select a Date </h3>
+          <div className='timeWrapper'>
+          <div>
+          <h3 className='timeText'> Select a Date </h3>
           <form>
             <select>
               <option>12/22/2018</option>
               <option>12/23/2018</option>
               <option>12/24/2018</option>
               <option>12/26/2018</option>
+              <option>12/27/2018</option>
+              <option>12/28/2018</option>
+              <option>12/29/2018</option>
+              <option>12/30/2018</option>
+              <option>12/31/2018</option>
             </select>
           </form>
-          <h3> Select a Start Time </h3>
+          </div>
+          <div>
+          <h3 className='timeText'> Select a Start Time </h3>
           <form>
             <select>
-              <option value='06:00AM'>06:00 AM</option>
-              <option value='07:00AM'>07:00 AM</option>
-              <option value='18:00AM'>08:00 AM</option>
-              <option value='19:00AM'>09:00 AM</option>
+              <option> 06:00 AM</option>
+              <option> 07:00 AM</option>
+              <option> 08:00 AM</option>
+              <option> 09:00 AM</option>
+              <option> 10:00 AM</option>
+              <option> 11:00 AM</option>
+              <option> 12:00 PM</option>
+              <option> 1:00 PM</option>
+              <option> 2:00 PM</option>
+              <option> 3:00 PM</option>
+              <option> 4:00 PM</option>
+              <option> 5:00 PM</option>
             </select>
           </form>
-          <h3> Select an End Time </h3>
+          </div>
+          <div>
+          <h3 className='timeText'> Select an End Time </h3>
           <form>
             <select>
-              <option value='06:00AM'>06:00 AM</option>
-              <option value='07:00AM'>07:00 AM</option>
-              <option value='18:00AM'>08:00 AM</option>
-              <option value='19:00AM'>09:00 AM</option>
+              <option> 06:00 AM</option>
+              <option> 07:00 AM</option>
+              <option> 08:00 AM</option>
+              <option> 09:00 AM</option>
+              <option> 10:00 AM</option>
+              <option> 11:00 AM</option>
+              <option> 12:00 PM</option>
+              <option> 1:00 PM</option>
+              <option> 2:00 PM</option>
+              <option> 3:00 PM</option>
+              <option> 4:00 PM</option>
+              <option> 5:00 PM</option>
             </select>
           </form>
-      
+          <button className='submitTime'> Confirm </button>
+          </div>
+          </div>
         </div>
       </div>
     )
