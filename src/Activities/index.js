28db0de8767863a357b0309bb83e07ad23b08091
Christine Samarchi <csamarchi@gmail.com>
2018-12-20 21:38:27 -0600

    completed location page

--- src/Activities/index.js
+++ src/Activities/index.js
@@ -12,46 +12,46 @@ class Activities extends Component {
     return (
       <div className='background'>
         <Nav />
-        <h1 className='header' > Choose an Activity </h1>
-        <div className='activityWrapper'>
+          <h1 className='header' > Choose an Activity </h1>
+            <div className='div'>
+              <div className='activityWrapper'>
+                <Link to ='/location'>
+                  <div className="plantTree">
+                    <h3 className='categoryButtonText'> Plant a tree </h3>
+                  </div>
+                </Link>
+                <Link to ='/location'>
+                  <div className="waterTree">
+                    <h3 className='categoryButtonText'> Water Tree </h3>
+                  </div>
+                </Link>
 
-          <Link to ='/time'>
-            <div className="plantTree">
-              <h3 className='categoryButtonText'> Plant a tree </h3>
+                <Link to ='/location'>
+                  <div className="pickFruit">
+                    <h3 className='categoryButtonText'> Pick Fruit </h3>
+                  </div>
+                </Link>
+              </div>
+                <div className='activityWrapper2'>
+                  <Link to ='/location'>
+                    <div className="market">
+                      <h3 className='categoryButtonText'> Farmers Market </h3>
+                    </div>
+                  </Link>
+
+                  <Link to ='/location'>
+                    <div className="maintenance">
+                      <h3 className='categoryButtonText'> Tree Maintenance</h3>
+                    </div>
+                  </Link>
+
+                  <Link to ='/location'>
+                    <div className="transport">
+                      <h3 className='categoryButtonText'> Transport</h3>
+                    </div>
+                  </Link>
+                </div>
             </div>
-          </Link>
-
-          <Link to ='/time'>
-            <div className="waterTree">
-              <h3 className='categoryButtonText'> Water Tree </h3>
-            </div>
-          </Link>
-
-          <Link to ='/time'>
-            <div className="pickFruit">
-              <h3 className='categoryButtonText'> Pick Fruit </h3>
-            </div>
-          </Link>
-
-          <Link to ='/time'>
-            <div className="market">
-              <h3 className='categoryButtonText'> Farmers Market </h3>
-            </div>
-          </Link>
-
-          <Link to ='/time'>
-            <div className="maintenance">
-              <h3 className='categoryButtonText'> Tree Maintenance</h3>
-            </div>
-          </Link>
-
-          <Link to ='/time'>
-            <div className="transport">
-              <h3 className='categoryButtonText'> Transport</h3>
-            </div>
-          </Link>
-
-        </div>
       </div>
     )
   }
--- src/Location/index.js
+++ src/Location/index.js
@@ -12,10 +12,49 @@ class Location extends Component {
     return (
       <div className='background'>
         <Nav />
-        <div>
           <h1 className='header' > Choose a Location </h1>
+          <div className='div'>
 
-        </div>
+            <div className='locationWrapper'>
+              <Link to ='/time'>
+                <div className="gillis">
+                  <h3 className='locationButtonText'> Gillis Neighborhood Park </h3>
+                </div>
+              </Link>
+
+              <Link to ='/time'>
+                <div className="auditorium">
+                  <h3 className='locationButtonText'> Auditorium Shores at Town Lake </h3>
+                </div>
+              </Link>
+
+              <Link to ='/time'>
+                <div className="stacy">
+                  <h3 className='locationButtonText'> Little Stacy Neighborhood Park </h3>
+                </div>
+              </Link>
+            </div>
+
+              <div className='locationWrapper2'>
+                <Link to ='/time'>
+                  <div className="nicholas">
+                    <h3 className='locationButtonText'> Nicholas Dawson Neighborhood Park</h3>
+                  </div>
+                </Link>
+
+                <Link to ='/time'>
+                  <div className="republic">
+                    <h3 className='locationButtonText'> Republic Square</h3>
+                  </div>
+                </Link>
+
+                <Link to ='/time'>
+                  <div className="waller">
+                    <h3 className='locationButtonText'> Waller Beach at Town Lake</h3>
+                  </div>
+                </Link>
+              </div>
+          </div>
       </div>
     )
   }
